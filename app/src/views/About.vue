eb15bbf001e7e5ef5c9b98262d0dc98702f1feb5
Jared Dunn <jdunn@webucator.com>
2022-07-12 15:29:16 -0400

    chore: create views

---
+++ app/src/views/About.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <h1>About</h1>
+  <nav class="flex bg-gray-100 m-5">
+    <router-link to="/">Home</router-link>
+  </nav>
+</template>
---
+++ app/src/views/PageNotFound.vue
@@ -0,0 +1,6 @@
+<template>
+    Page Not Found
+    <router-link to="/">Return Home</router-link>
+</template>
+
+<script setup></script>

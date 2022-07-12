e7012523bebdf59358d9baa25c35e9a662a26e13
Jared Dunn <jdunn@webucator.com>
2022-07-12 10:20:17 -0400

    chore: build, deploy, and change project id

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("5Dy4o72t49Mvv9nQbajT8EhLuyfXafAzQcANnEDczZMS");
 
 #[program]
 pub mod solana_twitter {

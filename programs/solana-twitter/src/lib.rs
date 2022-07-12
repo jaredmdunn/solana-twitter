1298868107929aa12fd77a957684da25a79d2616
Jared Dunn <jdunn@webucator.com>
2022-07-12 11:04:34 -0400

    feat: add send_tweet fn to solana_twitter mod

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
 
 declare_id!("5Dy4o72t49Mvv9nQbajT8EhLuyfXafAzQcANnEDczZMS");
 
@@ -6,18 +7,45 @@ declare_id!("5Dy4o72t49Mvv9nQbajT8EhLuyfXafAzQcANnEDczZMS");
 pub mod solana_twitter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> Result<()> {
+        // access tweet account
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
+        // access author account
+        let author: &Signer = &ctx.accounts.author;
+        // get Rust Clock object to get current timestamp
+        let clock: Clock = Clock::get().unwrap();
+
+        // check that topic is less than 50 chars
+        if topic.chars().count() > 50 {
+            // might need `return Err(error!(ErrorCode::TopicTooLong));` instead
+            return Err(ErrorCode::TopicTooLong.into());
+        }
+
+        // check that content is less than 280 chars
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into());
+        }
+
+        // * dereferences author.key
+        // set tweet author
+        tweet.author = *author.key;
+        // set tweet timestamp
+        tweet.timestamp = clock.unix_timestamp;
+        // set topic and content
+        tweet.topic = topic;
+        tweet.content = content;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
-
+#[account]
 pub struct Tweet {
     pub author: Pubkey,
-    pub timestamp: u64,
-    pub topics: String,
+    pub timestamp: i64,
+    pub topic: String,
     pub content: String,
 }
 
@@ -41,4 +69,24 @@ impl Tweet {
         + TIMESTAMP_LENGTH // timestampt
         + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // topic
         + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // content
+}
+
+#[derive(Accounts)]
+pub struct SendTweet<'info> {
+    // instruction on how to initialize tweet account
+    #[account(init, payer = author, space = Tweet::LEN)]
+    pub tweet: Account<'info, Tweet>,
+    // author needs to be mutable because we will mutate the amount of money in the account
+    #[account(mut)]
+    pub author: Signer<'info>,
+    // System variable ensures that system_program is official Solana System Program
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
 }

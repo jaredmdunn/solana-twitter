15867b4d74e222da36271f6c6cf513a0f232a77e
Jared Dunn <jdunn@webucator.com>
2022-07-12 15:29:02 -0400

    chore: set up router and import css

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+// CSS
+import './main.css';
+import 'solana-wallets-vue/styles.css';
+
+import routes from './routes';
+
+import App from './App.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount('#app');

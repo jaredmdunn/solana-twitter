a9cb461582425e6c77a6e30077e2c9edf3f74a82
Jared Dunn <jdunn@webucator.com>
2022-07-13 14:06:30 -0400

    Change routes

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,7 +1,7 @@
 const ROOT_URL = process.env.VUE_APP_ROOT_PATH;
 
 const routes = [
-  { path: ROOT_URL + '/', component: () => import('./views/Home.vue') },
+  { path: ROOT_URL, component: () => import('./views/Home.vue') },
 ];
 
 export default routes;

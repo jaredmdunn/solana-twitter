4308dd3e97b6185d318f4486690b7c86f1ec5d72
Jared Dunn <jdunn@webucator.com>
2022-07-13 11:43:35 -0400

    refactor: add commitment to provider, move tweet stuff into tweets.js

--- app/src/workspace.js
+++ app/src/workspace.js
@@ -1,19 +1,22 @@
 import { computed } from 'vue';
 import { useAnchorWallet } from 'solana-wallets-vue';
 import { Connection, PublicKey } from '@solana/web3.js';
-import { AnchorProvider, Program } from '@project-serum/anchor';
+import { Program, AnchorProvider } from '@project-serum/anchor';
 import idl from '../../target/idl/solana_twitter.json';
 
 
 const programID = new PublicKey(idl.metadata.address);
 let workspace = null;
 
+const preflightCommitment = 'processed';
+const commitment = 'processed';
+
 export const useWorkspace = () => workspace;
 
 export const initWorkspace = () => {
   const wallet = useAnchorWallet();
   const connection = new Connection('http://127.0.0.1:8899');
-  const provider = computed(() => new AnchorProvider(connection, wallet.value));
+  const provider = computed(() => new AnchorProvider(connection, wallet.value, { preflightCommitment, commitment }));
   const program = computed(() => new Program(idl, programID, provider.value));
 
   workspace = {
@@ -22,10 +25,4 @@ export const initWorkspace = () => {
     provider,
     program,
   };
-};
-
-export const fetchTweets = async () => {
-  const { program } = useWorkspace();
-  const tweets = program.value.account.tweet.all();
-  return tweets;
 };

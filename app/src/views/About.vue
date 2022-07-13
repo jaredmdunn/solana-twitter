caeeb70206d00db07ead6347d916ad02b391abd0
Jared Dunn <jdunn@webucator.com>
2022-07-13 13:53:31 -0400

    delete unused pages

--- app/src/views/About.vue
+++
@@ -1,8 +0,0 @@
-<script setup></script>
-
-<template>
-  <h1>About</h1>
-  <nav class="flex bg-gray-100 m-5">
-    <router-link to="/">Home</router-link>
-  </nav>
-</template>
--- app/src/views/PageNotFound.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    Page Not Found
-    <router-link to="/">Return Home</router-link>
-</template>
-
-<script setup></script>

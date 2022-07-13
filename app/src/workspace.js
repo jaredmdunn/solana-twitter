a820ba5565aeaa460e573a44317b2fe6fb62b670
Jared Dunn <jdunn@webucator.com>
2022-07-13 13:19:27 -0400

    feat: update workspace to use env variables

--- app/src/workspace.js
+++ app/src/workspace.js
@@ -2,12 +2,15 @@ import { computed } from 'vue';
 import { useAnchorWallet } from 'solana-wallets-vue';
 import { Connection, PublicKey } from '@solana/web3.js';
 import { Program, AnchorProvider } from '@project-serum/anchor';
-import idl from '../../target/idl/solana_twitter.json';
-
+// import idl from '../../target/idl/solana_twitter.json';
+import idl from './idl/solana_twitter.json';
+// import { IDL } from "./idl/solana_twitter";
 
 const programID = new PublicKey(idl.metadata.address);
+// const programID = new PublicKey(IDL.);
 let workspace = null;
 
+const clusterUrl = process.env.VUE_APP_CLUSTER_URL;
 const preflightCommitment = 'processed';
 const commitment = 'processed';
 
@@ -15,7 +18,7 @@ export const useWorkspace = () => workspace;
 
 export const initWorkspace = () => {
   const wallet = useAnchorWallet();
-  const connection = new Connection('http://127.0.0.1:8899');
+  const connection = new Connection(clusterUrl, commitment);
   const provider = computed(() => new AnchorProvider(connection, wallet.value, { preflightCommitment, commitment }));
   const program = computed(() => new Program(idl, programID, provider.value));
 
